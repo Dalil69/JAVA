292bad3cf480069d54371ec5581fb324b800fa57
John Doe <johndoe@example.com>
2023-07-12 09:43:53 +0200

    comtoeat

---
+++ Exercicetrois/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.time.LocalTime;
+public class Exercice3 {
+    public static void main(String[] args) {
+        Scanner jesaispasfrr = new Scanner(System.in);
+        Random pif = new Random();
+        int essais = 0;
+        LocalTime.now();
+
+        while
+    }
+}
