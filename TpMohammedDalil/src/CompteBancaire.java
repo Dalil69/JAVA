b44fc4df6a110b372570e01d4edf4d685fa43d9c
John Doe <johndoe@example.com>
2023-07-12 14:26:57 +0200

    Ahouiouioui

--- TpMohammedDalil/src/CompteBancaire.java
+++ TpMohammedDalil/src/CompteBancaire.java
@@ -1,31 +1,31 @@
 public class CompteBancaire {
-    private String nom;
-    private int codePIN;
-    private double solde;
+    private String nom; // Nom du titulaire du compte
+    private int codePIN; // Code PIN du compte
+    private double solde; // Solde du compte
 
     public CompteBancaire(String nom, int codePIN, double soldeInitial) {
-        this.nom = nom;
-        this.codePIN = codePIN;
-        this.solde = soldeInitial;
+        this.nom = nom; // Initialise le nom du titulaire du compte
+        this.codePIN = codePIN; // Initialise le code PIN du compte
+        this.solde = soldeInitial; // Initialise le solde initial du compte
     }
 
     public String getNom() {
-        return nom;
+        return nom; // Renvoie le nom du titulaire du compte
     }
 
     public int getCodePIN() {
-        return codePIN;
+        return codePIN; // Renvoie le code PIN du compte
     }
 
     public double getSolde() {
-        return solde;
+        return solde; // Renvoie le solde du compte
     }
 
     public void retirer(double montant) {
-        solde -= montant;
+        solde -= montant; // Réduit le solde du compte de la valeur du montant spécifié
     }
 
     public void deposer(double montant) {
-        solde += montant;
+        solde += montant; // Augmente le solde du compte de la valeur du montant spécifié
     }
 }
